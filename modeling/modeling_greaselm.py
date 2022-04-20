36eb65f463b08aa893d4cdb0eacd07e83d1a97a1
Vladimir Blagojevic <dovlex@gmail.com>
2022-04-20 13:35:03 +0200

    Move layers modules to modeling_greaselm.py

--- modeling/modeling_greaselm.py
+++ modeling/modeling_greaselm.py
@@ -1,6 +1,8 @@
 import logging
+import math
 import os
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,7 +19,7 @@ from transformers.file_utils import (
 )
 
 from modeling import modeling_gnn
-from utils import layers
+
 logger = logging.getLogger(__name__)
 
 
@@ -27,6 +29,187 @@ else:
     ModelClass = modeling_roberta.RobertaModel
 
 print ('ModelClass', ModelClass)
+
+
+def freeze_net(module):
+    for p in module.parameters():
+        p.requires_grad = False
+
+
+def gelu(x):
+    """ Implementation of the gelu activation function currently in Google Bert repo (identical to OpenAI GPT).
+        Also see https://arxiv.org/abs/1606.08415
+    """
+    return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+
+# Todo: replace with transformers new gelu
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return gelu(x)
+
+
+class CustomizedEmbedding(nn.Module):
+    def __init__(self, concept_num, concept_in_dim, concept_out_dim, use_contextualized=False,
+                 pretrained_concept_emb=None, freeze_ent_emb=True, scale=1.0, init_range=0.02):
+        super().__init__()
+        self.scale = scale
+        self.use_contextualized = use_contextualized
+        if not use_contextualized:
+            self.emb = nn.Embedding(concept_num + 2, concept_in_dim)
+            if pretrained_concept_emb is not None:
+                self.emb.weight.data.fill_(0)
+                self.emb.weight.data[:concept_num].copy_(pretrained_concept_emb)
+            else:
+                self.emb.weight.data.normal_(mean=0.0, std=init_range)
+            if freeze_ent_emb:
+                freeze_net(self.emb)
+
+        if concept_in_dim != concept_out_dim:
+            self.cpt_transform = nn.Linear(concept_in_dim, concept_out_dim)
+            self.activation = GELU()
+
+    def forward(self, index, contextualized_emb=None):
+        """
+        index: size (bz, a)
+        contextualized_emb: size (bz, b, emb_size) (optional)
+        """
+        if contextualized_emb is not None:
+            assert index.size(0) == contextualized_emb.size(0)
+            if hasattr(self, 'cpt_transform'):
+                contextualized_emb = self.activation(self.cpt_transform(contextualized_emb * self.scale))
+            else:
+                contextualized_emb = contextualized_emb * self.scale
+            emb_dim = contextualized_emb.size(-1)
+            return contextualized_emb.gather(1, index.unsqueeze(-1).expand(-1, -1, emb_dim))
+        else:
+            if hasattr(self, 'cpt_transform'):
+                return self.activation(self.cpt_transform(self.emb(index) * self.scale))
+            else:
+                return self.emb(index) * self.scale
+
+
+class MatrixVectorScaledDotProductAttention(nn.Module):
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, q, k, v, mask=None):
+        """
+        q: tensor of shape (n*b, d_k)
+        k: tensor of shape (n*b, l, d_k)
+        v: tensor of shape (n*b, l, d_v)
+
+        returns: tensor of shape (n*b, d_v), tensor of shape(n*b, l)
+        """
+        attn = (q.unsqueeze(1) * k).sum(2)  # (n*b, l)
+        attn = attn / self.temperature
+        if mask is not None:
+            attn = attn.masked_fill(mask, -np.inf)
+        attn = self.softmax(attn)
+        attn = self.dropout(attn)
+        output = (attn.unsqueeze(2) * v).sum(1)
+        return output, attn
+
+
+class MultiheadAttPoolLayer(nn.Module):
+
+    def __init__(self, n_head, d_q_original, d_k_original, dropout=0.1):
+        super().__init__()
+        assert d_k_original % n_head == 0  # make sure the outpute dimension equals to d_k_origin
+        self.n_head = n_head
+        self.d_k = d_k_original // n_head
+        self.d_v = d_k_original // n_head
+
+        self.w_qs = nn.Linear(d_q_original, n_head * self.d_k)
+        self.w_ks = nn.Linear(d_k_original, n_head * self.d_k)
+        self.w_vs = nn.Linear(d_k_original, n_head * self.d_v)
+
+        nn.init.normal_(self.w_qs.weight, mean=0, std=np.sqrt(2.0 / (d_q_original + self.d_k)))
+        nn.init.normal_(self.w_ks.weight, mean=0, std=np.sqrt(2.0 / (d_k_original + self.d_k)))
+        nn.init.normal_(self.w_vs.weight, mean=0, std=np.sqrt(2.0 / (d_k_original + self.d_v)))
+
+        self.attention = MatrixVectorScaledDotProductAttention(temperature=np.power(self.d_k, 0.5))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, mask=None):
+        """
+        q: tensor of shape (b, d_q_original)
+        k: tensor of shape (b, l, d_k_original)
+        mask: tensor of shape (b, l) (optional, default None)
+        returns: tensor of shape (b, n*d_v)
+        """
+        n_head, d_k, d_v = self.n_head, self.d_k, self.d_v
+
+        bs, _ = q.size()
+        bs, len_k, _ = k.size()
+
+        qs = self.w_qs(q).view(bs, n_head, d_k)  # (b, n, dk)
+        ks = self.w_ks(k).view(bs, len_k, n_head, d_k)  # (b, l, n, dk)
+        vs = self.w_vs(k).view(bs, len_k, n_head, d_v)  # (b, l, n, dv)
+
+        qs = qs.permute(1, 0, 2).contiguous().view(n_head * bs, d_k)
+        ks = ks.permute(2, 0, 1, 3).contiguous().view(n_head * bs, len_k, d_k)
+        vs = vs.permute(2, 0, 1, 3).contiguous().view(n_head * bs, len_k, d_v)
+
+        if mask is not None:
+            mask = mask.repeat(n_head, 1)
+        output, attn = self.attention(qs, ks, vs, mask=mask)
+
+        output = output.view(n_head, bs, d_v)
+        output = output.permute(1, 0, 2).contiguous().view(bs, n_head * d_v)  # (b, n*dv)
+        output = self.dropout(output)
+        return output, attn
+
+
+class MLP(nn.Module):
+    """
+    Multi-layer perceptron
+
+    Parameters
+    ----------
+    num_layers: number of hidden layers
+    """
+    activation_classes = {'gelu': GELU, 'relu': nn.ReLU, 'tanh': nn.Tanh}
+
+    def __init__(self, input_size, hidden_size, output_size, num_layers, dropout, batch_norm=False,
+                 init_last_layer_bias_to_zero=False, layer_norm=False, activation='gelu'):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.batch_norm = batch_norm
+        self.layer_norm = layer_norm
+
+        assert not (self.batch_norm and self.layer_norm)
+
+        self.layers = nn.Sequential()
+        for i in range(self.num_layers + 1):
+            n_in = self.input_size if i == 0 else self.hidden_size
+            n_out = self.hidden_size if i < self.num_layers else self.output_size
+            self.layers.add_module(f'{i}-Linear', nn.Linear(n_in, n_out))
+            if i < self.num_layers:
+                self.layers.add_module(f'{i}-Dropout', nn.Dropout(self.dropout))
+                if self.batch_norm:
+                    self.layers.add_module(f'{i}-BatchNorm1d', nn.BatchNorm1d(self.hidden_size))
+                if self.layer_norm:
+                    self.layers.add_module(f'{i}-LayerNorm', nn.LayerNorm(self.hidden_size))
+                self.layers.add_module(f'{i}-{activation}', self.activation_classes[activation.lower()]())
+        if init_last_layer_bias_to_zero:
+            self.layers[-1].bias.data.fill_(0)
+
+    def forward(self, input):
+        return self.layers(input)
+
 
 
 class GreaseLM(nn.Module):
@@ -166,16 +349,16 @@ class LMGNN(nn.Module):
         self.k = k
         self.concept_dim = concept_dim
         self.n_attention_head = n_attention_head
-        self.activation = layers.GELU()
+        self.activation = GELU()
         if k >= 0:
-            self.concept_emb = layers.CustomizedEmbedding(concept_num=n_concept, concept_out_dim=concept_dim,
-                                                          use_contextualized=False, concept_in_dim=concept_in_dim,
-                                                          pretrained_concept_emb=pretrained_concept_emb,
-                                                          freeze_ent_emb=freeze_ent_emb)
-            self.pooler = layers.MultiheadAttPoolLayer(n_attention_head, config.hidden_size, concept_dim)
+            self.concept_emb = CustomizedEmbedding(concept_num=n_concept, concept_out_dim=concept_dim,
+                                                   use_contextualized=False, concept_in_dim=concept_in_dim,
+                                                   pretrained_concept_emb=pretrained_concept_emb,
+                                                   freeze_ent_emb=freeze_ent_emb)
+            self.pooler = MultiheadAttPoolLayer(n_attention_head, config.hidden_size, concept_dim)
 
         concat_vec_dim = concept_dim * 2 + config.hidden_size
-        self.fc = layers.MLP(concat_vec_dim, fc_dim, 1, n_fc_layer, p_fc, layer_norm=True)
+        self.fc = MLP(concat_vec_dim, fc_dim, 1, n_fc_layer, p_fc, layer_norm=True)
 
         self.dropout_e = nn.Dropout(p_emb)
         self.dropout_fc = nn.Dropout(p_fc)
@@ -335,7 +518,7 @@ class TextKGMessagePassing(ModelClass):
         self.Vh = nn.Linear(concept_dim, concept_dim)
         self.Vx = nn.Linear(concept_dim, concept_dim)
 
-        self.activation = layers.GELU()
+        self.activation = GELU()
         self.dropout = nn.Dropout(dropout)
         self.dropout_rate = dropout
 
@@ -789,15 +972,15 @@ class RoBERTaGAT(modeling_bert.BertEncoder):
                                                 torch.nn.BatchNorm1d(hidden_size), torch.nn.ReLU(),
                                                 torch.nn.Linear(hidden_size, hidden_size))
         self.gnn_layers = nn.ModuleList([modeling_gnn.GATConvE(hidden_size, n_ntype, n_etype, self.edge_encoder) for _ in range(k)])
-        self.activation = layers.GELU()
+        self.activation = GELU()
         self.dropout_rate = dropout
 
         self.sent_dim = config.hidden_size
         self.sep_ie_layers = sep_ie_layers
         if sep_ie_layers:
-            self.ie_layers = nn.ModuleList([layers.MLP(self.sent_dim + concept_dim, ie_dim, self.sent_dim + concept_dim, ie_layer_num, p_fc) for _ in range(k)])
+            self.ie_layers = nn.ModuleList([MLP(self.sent_dim + concept_dim, ie_dim, self.sent_dim + concept_dim, ie_layer_num, p_fc) for _ in range(k)])
         else:
-            self.ie_layer = layers.MLP(self.sent_dim + concept_dim, ie_dim, self.sent_dim + concept_dim, ie_layer_num, p_fc)
+            self.ie_layer = MLP(self.sent_dim + concept_dim, ie_dim, self.sent_dim + concept_dim, ie_layer_num, p_fc)
 
         self.concept_dim = concept_dim
         self.num_hidden_layers = config.num_hidden_layers
